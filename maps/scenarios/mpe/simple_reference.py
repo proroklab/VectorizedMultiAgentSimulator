f41ce828eca9fd8ad0768eb1610286d14b265e64
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-05-15 10:04:57 +0100

    Implemented speaker listener

--- maps/scenarios/mpe/simple_reference.py
+++ maps/scenarios/mpe/simple_reference.py
@@ -1,81 +1,117 @@
-import numpy as np
-from multiagent.core import World, Agent, Landmark
-from multiagent.scenario import BaseScenario
+import torch
+
+from maps.core import World, Agent, Landmark
+from maps.scenario import BaseScenario
+
 
 class Scenario(BaseScenario):
-    def make_world(self):
-        world = World()
-        # set any world properties first
-        world.dim_c = 10
-        world.collaborative = True  # whether agents share rewards
-        # add agents
-        world.agents = [Agent() for i in range(2)]
-        for i, agent in enumerate(world.agents):
-            agent.name = 'agent %d' % i
-            agent.collide = False
-        # add landmarks
-        world.landmarks = [Landmark() for i in range(3)]
-        for i, landmark in enumerate(world.landmarks):
-            landmark.name = 'landmark %d' % i
-            landmark.collide = False
-            landmark.movable = False
-        # make initial conditions
-        self.reset_world(world)
+    def make_world(self, batch_dim: int, device: torch.device, **kwargs):
+        world = World(batch_dim=batch_dim, device=device, dim_c=10)
+
+        n_agents = 2
+        n_landmarks = 3
+
+        # Add agents
+        for i in range(n_agents):
+            agent = Agent(name=f"agent {i}", collide=False, silent=False)
+            world.add_agent(agent)
+        # Add landmarks
+        for i in range(n_landmarks):
+            landmark = Landmark(
+                name=f"landmark {i}",
+                collide=False,
+            )
+            world.add_landmark(landmark)
+
         return world
 
-    def reset_world(self, world):
-        # assign goals to agents
-        for agent in world.agents:
-            agent.goal_a = None
-            agent.goal_b = None
-        # want other agent to go to the goal landmark
-        world.agents[0].goal_a = world.agents[1]
-        world.agents[0].goal_b = np.random.choice(world.landmarks)
-        world.agents[1].goal_a = world.agents[0]
-        world.agents[1].goal_b = np.random.choice(world.landmarks)
-        # random properties for agents
-        for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.25,0.25,0.25])               
-        # random properties for landmarks
-        world.landmarks[0].color = np.array([0.75,0.25,0.25]) 
-        world.landmarks[1].color = np.array([0.25,0.75,0.25]) 
-        world.landmarks[2].color = np.array([0.25,0.25,0.75]) 
-        # special colors for goals
-        world.agents[0].goal_a.color = world.agents[0].goal_b.color                
-        world.agents[1].goal_a.color = world.agents[1].goal_b.color                               
-        # set random initial states
-        for agent in world.agents:
-            agent.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
-            agent.state.p_vel = np.zeros(world.dim_p)
-            agent.state.c = np.zeros(world.dim_c)
-        for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = np.random.uniform(-1,+1, world.dim_p)
-            landmark.state.p_vel = np.zeros(world.dim_p)
+    def reset_world_at(self, env_index: int = None):
+        if env_index is None:
+            # assign goals to agents
+            for agent in self.world.agents:
+                agent.goal_a = None
+                agent.goal_b = None
+            # want other agent to go to the goal landmark
+            self.world.agents[0].goal_a = self.world.agents[1]
+            self.world.agents[0].goal_b = self.world.landmarks[
+                torch.randint(0, len(self.world.landmarks), (1,)).item()
+            ]
+            self.world.agents[1].goal_a = self.world.agents[0]
+            self.world.agents[1].goal_b = self.world.landmarks[
+                torch.randint(0, len(self.world.landmarks), (1,)).item()
+            ]
+            # random properties for agents
+            for i, agent in enumerate(self.world.agents):
+                agent.color = torch.tensor(
+                    [0.25, 0.25, 0.25], device=self.world.device, dtype=torch.float64
+                )
+            # random properties for landmarks
+            self.world.landmarks[0].color = torch.tensor(
+                [0.75, 0.25, 0.25], device=self.world.device, dtype=torch.float64
+            )
+            self.world.landmarks[1].color = torch.tensor(
+                [0.25, 0.75, 0.25], device=self.world.device, dtype=torch.float64
+            )
+            self.world.landmarks[2].color = torch.tensor(
+                [0.25, 0.25, 0.75], device=self.world.device, dtype=torch.float64
+            )
+            # special colors for goals
+            self.world.agents[0].goal_a.color = self.world.agents[0].goal_b.color
+            self.world.agents[1].goal_a.color = self.world.agents[1].goal_b.color
 
-    def reward(self, agent, world):
+        # set random initial states
+        for agent in self.world.agents:
+            agent.set_pos(
+                2
+                * torch.rand(
+                    self.world.dim_p, device=self.world.device, dtype=torch.float64
+                )
+                - 1,
+                batch_index=env_index,
+            )
+        for landmark in self.world.landmarks:
+            landmark.set_pos(
+                2
+                * torch.rand(
+                    self.world.dim_p, device=self.world.device, dtype=torch.float64
+                )
+                - 1,
+                batch_index=env_index,
+            )
+
+    def reward(self, agent: Agent):
         if agent.goal_a is None or agent.goal_b is None:
-            return 0.0
-        dist2 = np.sum(np.square(agent.goal_a.state.p_pos - agent.goal_b.state.p_pos))
+            return torch.zeros(
+                self.world.batch_dim, device=self.world.device, dtype=torch.float64
+            )
+        dist2 = torch.sqrt(
+            torch.sum(
+                torch.square(agent.goal_a.state.pos - agent.goal_b.state.pos), dim=-1
+            )
+        )
         return -dist2
 
-    def observation(self, agent, world):
+    def observation(self, agent: Agent):
         # goal color
-        goal_color = [np.zeros(world.dim_color), np.zeros(world.dim_color)]
-        if agent.goal_b is not None:
-            goal_color[1] = agent.goal_b.color 
+        goal_color = agent.goal_b.color
 
         # get positions of all entities in this agent's reference frame
         entity_pos = []
-        for entity in world.landmarks:
-            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
-        # entity colors
-        entity_color = []
-        for entity in world.landmarks:
-            entity_color.append(entity.color)
+        for entity in self.world.landmarks:
+            entity_pos.append(entity.state.pos - agent.state.pos)
+
         # communication of all other agents
         comm = []
-        for other in world.agents:
-            if other is agent: continue
+        for other in self.world.agents:
+            if other is agent:
+                continue
             comm.append(other.state.c)
-        return np.concatenate([agent.state.p_vel] + entity_pos + [goal_color[1]] + comm)
-            
+        return torch.cat(
+            [
+                agent.state.vel,
+                *entity_pos,
+                goal_color.repeat(self.world.batch_dim, 1),
+                *comm,
+            ],
+            dim=-1,
+        )
