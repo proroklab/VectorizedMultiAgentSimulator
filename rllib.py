cc685ef96fc8d9d88cfac14b7ce6d1ab161f0b3e
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-05-25 18:28:01 -0400

    Rllib example

--- rllib.py
+++ rllib.py
@@ -2,15 +2,13 @@
 #  All rights reserved.
 
 import os
-import platform
-from pathlib import Path
 from typing import Dict, Optional
 
 import numpy as np
 import ray
 from ray import tune
 from ray.rllib import RolloutWorker, BaseEnv, Policy
-from ray.rllib.agents import DefaultCallbacks, MultiCallbacks
+from ray.rllib.agents import DefaultCallbacks
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.evaluation import Episode
 from ray.rllib.utils.typing import PolicyID
@@ -64,27 +62,31 @@ class RenderingCallbacks(DefaultCallbacks):
         episode: Episode,
         **kwargs,
     ) -> None:
-        print(self.frames)
         vid = np.transpose(self.frames, (0, 3, 1, 2))
-        episode.media["rendering"] = wandb.Video(vid, fps=10, format="mp4")
+        episode.media["rendering"] = wandb.Video(vid, fps=30, format="mp4")
         self.frames = []
 
 
-scratch_dir = (
-    Path("/Users/Matteo/scratch/")
-    if platform.system() == "Darwin"
-    else Path("/local/scratch/mb2389/")
-)
-
-
 def train():
+
+    scenario_name = "waterfall"
+
+    continuous_actions = False
+    max_steps = 100
+    num_vectorized_envs = 32
+    num_workers = 7
+    maps_device = "cpu"
+    RLLIB_NUM_GPUS = int(os.environ.get("RLLIB_NUM_GPUS", "0"))
+    num_gpus = min(RLLIB_NUM_GPUS - 0.5, 0)  # Driver GPU
+    num_gpus_per_worker = (1 - num_gpus) / num_workers if maps_device == "cuda" else 0
+
     tune.run(
         PPOTrainer,
         stop={"training_iteration": 5000},
         callbacks=[
             WandbLoggerCallback(
                 project=f"maps_test",
-                api_key_file=f"{scratch_dir}/wandb_api_key_file",
+                api_key="",
             )
         ],
         config={
@@ -94,46 +96,44 @@ def train():
             "kl_coeff": 0.01,
             "kl_target": 0.01,
             "lambda": 0.99,
-            "clip_param": 0.2,  # 0.3
+            "clip_param": 0.2,
             "vf_loss_coeff": 1,
             "vf_clip_param": float("inf"),
-            "entropy_coeff": 0,  # 0.01,
+            "entropy_coeff": 0,
             "train_batch_size": 10000,
             "rollout_fragment_length": 200,
             "sgd_minibatch_size": 2000,
             "num_sgd_iter": 32,
-            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-            "num_workers": 5,
-            "num_gpus_per_worker": 0,
-            "num_envs_per_worker": 32,
+            "num_gpus": num_gpus,
+            "num_workers": num_workers,
+            "num_gpus_per_worker": num_gpus_per_worker,
+            "num_envs_per_worker": num_vectorized_envs,
             "lr": 5e-5,
-            "gamma": 0.999,
+            "gamma": 0.99,
             "use_gae": True,
             "use_critic": True,
             "batch_mode": "truncate_episodes",
             "env_config": {
-                "device": "cpu",
-                "num_envs": 32,
-                "scenario_name": "waterfall",
-                "continuous_actions": True,
-                "max_steps": 100,
+                "device": maps_device,
+                "num_envs": num_vectorized_envs,
+                "scenario_name": scenario_name,
+                "continuous_actions": continuous_actions,
+                "max_steps": max_steps,
             },
-            "evaluation_interval": 2,
+            "evaluation_interval": 50,
             "evaluation_duration": 1,
             "evaluation_num_workers": 1,
             "evaluation_parallel_to_training": True,
             "evaluation_config": {
                 "num_envs_per_worker": 1,
-                "callbacks": MultiCallbacks(
-                    [
-                        RenderingCallbacks,
-                    ]
-                ),
+                "callbacks": RenderingCallbacks,
+                "env_config": {
+                    "num_envs": 1,
+                },
             },
         },
     )
 
 
 if __name__ == "__main__":
-
     train()
