2cf7824b2204c27e7935ad2df4be53687b0790f4
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-06-08 17:22:58 +0100

    Update to simple_spread

--- maps/scenarios/mpe/simple_spread.py
+++ maps/scenarios/mpe/simple_spread.py
@@ -1,3 +1,6 @@
+#  Copyright (c) 2022. Matteo Bettini
+#  All rights reserved.
+
 import torch
 
 from maps.simulator.core import World, Agent, Landmark, Sphere
@@ -7,9 +10,13 @@ from maps.simulator.utils import Color
 
 class Scenario(BaseScenario):
     def make_world(self, batch_dim: int, device: torch.device, **kwargs):
+        num_agents = kwargs.get("n_agents", 3)
+        obs_agents = kwargs.get("obs_agents", True)
+
+        self.obs_agents = obs_agents
+
         world = World(batch_dim=batch_dim, device=device)
         # set any world properties first
-        num_agents = 3
         num_landmarks = num_agents
         # Add agents
         for i in range(num_agents):
@@ -54,7 +61,7 @@ class Scenario(BaseScenario):
 
     def is_collision(self, agent1: Agent, agent2: Agent):
         delta_pos = agent1.state.pos - agent2.state.pos
-        dist = torch.sqrt(torch.sum(torch.square(delta_pos), dim=-1))
+        dist = delta_pos.square().sum(-1).sqrt()
         dist_min = agent1.shape.radius + agent2.shape.radius
         return dist < dist_min
 
@@ -63,19 +70,22 @@ class Scenario(BaseScenario):
         rew = torch.zeros(
             self.world.batch_dim, device=self.world.device, dtype=torch.float32
         )
+        self._done = torch.full(self.world.batch_dim, True, device=self.world.device)
         for l in self.world.landmarks:
-            rew -= torch.min(
+            closest = torch.min(
                 torch.stack(
                     [
-                        torch.sqrt(
-                            torch.sum(torch.square(a.state.pos - l.state.pos), dim=-1)
-                        )
+                        (a.state.pos - l.state.pos).square().sum(-1).sqrt()
                         for a in self.world.agents
                     ],
                     dim=1,
                 ),
                 dim=-1,
             )[0]
+            goal_reached = closest < l.shape.radius
+            self._done *= goal_reached
+            rew -= closest
+
         if agent.collide:
             for a in self.world.agents:
                 rew[self.is_collision(a, agent)] -= 1
@@ -93,5 +103,14 @@ class Scenario(BaseScenario):
                 continue
             other_pos.append(other.state.pos - agent.state.pos)
         return torch.cat(
-            [agent.state.vel, agent.state.pos, *entity_pos, *other_pos], dim=-1
+            [
+                agent.state.pos,
+                agent.state.vel,
+                *entity_pos,
+                *(other_pos if self.obs_agents else []),
+            ],
+            dim=-1,
         )
+
+    def done(self):
+        return self._done
