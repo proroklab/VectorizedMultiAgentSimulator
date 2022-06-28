c1447a9fb5e6f220ddeef427c40a636e0ccb6753
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-06-28 18:04:27 +0100

    Formatting and linting

--- maps/scenarios/passage.py
+++ maps/scenarios/passage.py
@@ -276,9 +276,9 @@ class Scenario(BaseScenario):
             for a in self.world.agents:
                 if a != agent:
                     self.rew[self.world.is_overlapping(a, agent)] -= 10
-            for l in self.world.landmarks[self.n_agents : -4]:
-                if l.collide:
-                    self.rew[self.world.is_overlapping(agent, l)] -= 10
+            for landmark in self.world.landmarks[self.n_agents : -4]:
+                if landmark.collide:
+                    self.rew[self.world.is_overlapping(agent, landmark)] -= 10
 
         return self.rew
 
