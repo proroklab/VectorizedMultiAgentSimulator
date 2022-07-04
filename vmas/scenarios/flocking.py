ecff6c042a9a19fdca8372b66484d58de18adc76
J. Blumenkamp <jb2270@cam.ac.uk>
2022-07-04 14:11:16 +0100

    Add 5 randomly placed obstacles and lidar

--- vmas/scenarios/flocking.py
+++ vmas/scenarios/flocking.py
@@ -1,15 +1,16 @@
 #  Copyright (c) 2022. Jan Blumenkamp
 #  All rights reserved.
 import math
-from typing import Dict
+from typing import Dict, Callable
 
 import torch
 from torch import Tensor
 
 from vmas import render_interactively
-from vmas.simulator.core import Agent, Landmark, Sphere, World
+from vmas.simulator.core import Agent, Landmark, Sphere, World, Entity, Box
 from vmas.simulator.scenario import BaseScenario
 from vmas.simulator.utils import Color
+from vmas.simulator.sensors import Lidar
 
 DEFAULT_ENERGY_COEFF = 0.02
 
@@ -24,9 +25,21 @@ class Scenario(BaseScenario):
         # Make world
         world = World(batch_dim, device)
         # Add agents
+        goal_entity_filter: Callable[[Entity], bool] = lambda e: e.name == "goal"
         for i in range(n_agents):
             # Constraint: all agents have same action range and multiplier
-            agent = Agent(name=f"agent {i}", collide=True)
+            agent = Agent(
+                name=f"agent {i}",
+                collide=True,
+                sensors=[
+                    Lidar(
+                        world,
+                        n_rays=12,
+                        max_range=0.5,
+                        entity_filter=goal_entity_filter,
+                    )
+                ],
+            )
             world.add_agent(agent)
 
         # Add landmarks
@@ -37,6 +50,18 @@ class Scenario(BaseScenario):
             color=Color.GREEN,
         )
         world.add_landmark(goal)
+
+        self.obstacles = []
+        for i in range(5):
+            obstacle = Landmark(
+                name=f"obstacle_{i}",
+                collide=True,
+                movable=False,
+                shape=Box(width=0.1, length=0.1),
+                color=Color.RED,
+            )
+            world.add_landmark(obstacle)
+            self.obstacles.append(obstacle)
 
         return world
 
@@ -53,6 +78,32 @@ class Scenario(BaseScenario):
                 ).uniform_(
                     -1.0,
                     1.0,
+                ),
+                batch_index=env_index,
+            )
+        for obstacle in self.obstacles:
+            # Random pos between -1 and 1
+            obstacle.set_pos(
+                torch.zeros(
+                    (1, self.world.dim_p)
+                    if env_index is not None
+                    else (self.world.batch_dim, self.world.dim_p),
+                    device=self.world.device,
+                    dtype=torch.float32,
+                ).uniform_(
+                    -1.0,
+                    1.0,
+                ),
+                batch_index=env_index,
+            )
+            obstacle.set_rot(
+                torch.zeros(
+                    (1,) if env_index is not None else (self.world.batch_dim,),
+                    device=self.world.device,
+                    dtype=torch.float32,
+                ).uniform_(
+                    -torch.pi,
+                    torch.pi,
                 ),
                 batch_index=env_index,
             )
@@ -92,6 +143,7 @@ class Scenario(BaseScenario):
                 agent.state.pos,
                 agent.state.vel,
                 self.world.landmarks[0].state.pos - agent.state.pos,
+                agent.sensors[0].measure(),
             ],
             dim=-1,
         )
