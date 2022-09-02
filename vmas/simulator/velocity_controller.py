c83b0bed97fa861888c007c59779dfd07a86ac51
Ajay <ajshank@users.noreply.github.com>
2022-09-02 19:59:25 +0100

    fixup integrator control, add support for windup cutoff, cleanup
    * add reset method
    * TODO: clamp tensor efficiently

--- vmas/simulator/velocity_controller.py
+++ vmas/simulator/velocity_controller.py
@@ -6,13 +6,24 @@ import numpy as np;
 import vmas.simulator.core
 import vmas.simulator.utils
 
-
+"""
+Implements PID controller for velocity targets found in agent.action.u.
+Two forms of the PID controller are implemented: standard, and parallel. The controller takes 3 params, which
+are interpreted differently based on the form.
+> Standard form: ctrl_params=[gain, intg_ts, derv_ts]
+                    intg_ts: rise time for integrator (err will be tolerated for this interval)
+                    derv_ts: seek time for derivative (err is predicted over this interval)
+                    These are specified in 1/dt scale (0.5 means 0.5/0.1==5sec)
+> Parallel form: ctrl_params=[kP, kI, kD]
+                    kI and kD have no simple physical meaning, but are related to standard form params.
+                    intg_ts = kP/kI and kD/kP = derv_ts
+"""
 class VelocityController:
     def __init__(self, agent: vmas.simulator.core.Agent, dt: float, ctrl_params=[1, 0, 0], pid_form="standard"):
         self.agent = agent
         self.dt = dt
-        # controller parameters: standard=[kP, intTs ,dervTs], parallel=[kP, kI, kD]
-        #    in parallel form, kI = kP/intTs and kD = kP*dervTs
+        # controller parameters: standard=[kP, intgTs ,dervTs], parallel=[kP, kI, kD]
+        #    in parallel form, kI = kP/intgTs and kD = kP*dervTs
         self.ctrl_gain = ctrl_params[0];    # kP
         if pid_form == "standard":
             self.integralTs = ctrl_params[1];
@@ -31,36 +42,36 @@ class VelocityController:
             self.use_integrator = False;
         else:
             self.use_integrator = True;
+            # set windup limit to 50% of agent's max force
+            fmax = self.agent.max_f if self.agent.max_f is not None else 2.0;
+            self.integrator_windup_cutoff = 0.5 * fmax * self.integralTs/(self.dt * self.ctrl_gain);
+            print( "Intg cutoff: ", self.integrator_windup_cutoff );
         
-        self.integrator_hist = 200;
-        self.integrator_windup_cutoff = 2;
         # containers for integral & derivative control
-        self.vel_errs = []
+        self.accum_errs = 0.0;
         self.prev_err = 0.0;
 
         # do other initialisation bits
-        self.initialise();
-    
-    def initialise(self):
-        # initialise containter for integrator only if I-gain is not strictly zero
-        #if self.use_integrator:
-        #    self.vel_errs = np.zeros(self.integrator_hist);
-        self.vel_errs = [];
+        self.reset();
 
     def reset(self):
-        self.vel_errs = np.zeros(self.integrator_hist);
+        self.accum_errs = 0.0;
+        self.prev_err = 0.0;
     
-    def integralError(self, err, _idx=[0]):
+    def integralError(self, err):
         if not self.use_integrator:
             return 0;
-        # update integrator container as a fast ring buffer
-        ### self.vel_errs[_idx[0]] = err;
-        ### _idx[0] = (_idx[0] + 1) % self.integrator_hist;
-        ### return (1.0/self.integralTs)*np.sum( self.vel_errs ) * self.dt;
-        if len( self.vel_errs ) > self.integrator_hist-1:
-            self.vel_errs.pop(0);
-        self.vel_errs.append( err );
-        return (1.0/self.integralTs) * torch.stack( self.vel_errs, dim=1 ).sum(dim=1) * self.dt;
+        # fixed-length history (not recommended):
+        ### if len( self.accum_errs ) > self.integrator_hist-1:
+        ###    self.accum_errs.pop(0);
+        ### self.accum_errs.append( err );
+        ### return (1.0/self.integralTs) * torch.stack( self.accum_errs, dim=1 ).sum(dim=1) * self.dt;
+        
+        self.accum_errs += ( self.dt * err );
+        #self.accum_errs = vmas.simulator.utils.clamp_tensor(self.accum_errs,\
+        #                                                 -self.integrator_windup_cutoff,\
+        #                                                 self.integrator_windup_cutoff);
+        return (1.0/self.integralTs) * self.accum_errs;
     
     def rateError(self, err):
         e = self.derivativeTs * (err - self.prev_err)/self.dt;
@@ -79,10 +90,10 @@ class VelocityController:
 
         # Clamping force to limits
         if self.agent.max_f is not None:
-            force = vmas.simulator.utils.clamp_with_norm(force, self.agent.max_f)
+            u = vmas.simulator.utils.clamp_with_norm(u, self.agent.max_f)
         if self.agent.f_range is not None:
-            force = torch.clamp(force, -self.agent.f_range, self.agent.f_range)
+            u = torch.clamp(u, -self.agent.f_range, self.agent.f_range)
 
         self.agent.action.u = u;
-        before_after = des_vel[0].tolist() + u[0].tolist() + cur_vel[0].tolist();
-        print( *before_after );
+        # before_after = des_vel[0].tolist() + u[0].tolist() + cur_vel[0].tolist();
+        # print( *before_after );
