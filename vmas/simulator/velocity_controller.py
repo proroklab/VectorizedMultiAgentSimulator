7982d29d73b6652c36b0878043c197a8f4a67972
Ajay <ajshank@users.noreply.github.com>
2022-09-02 15:28:07 +0100

    add support for PID controlled velocity targets

--- vmas/simulator/velocity_controller.py
+++ vmas/simulator/velocity_controller.py
@@ -2,28 +2,75 @@
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
 import torch
+import numpy as np;
 import vmas.simulator.core
 import vmas.simulator.utils
 
 
 class VelocityController:
-    def __init__(self, agent: vmas.simulator.core.Agent, dt: float):
+    def __init__(self, agent: vmas.simulator.core.Agent, dt: float, ctrl_params=[1, 0, 0], pid_form="standard"):
         self.agent = agent
         self.dt = dt
-        self.positions = []
-        self.velocities = []
+        # controller parameters: standard=[kP, intTs ,dervTs], parallel=[kP, kI, kD]
+        #    in parallel form, kI = kP/intTs and kD = kP*dervTs
+        self.ctrl_gain = ctrl_params[0];    # kP
+        if pid_form == "standard":
+            self.integralTs = ctrl_params[1];
+            self.derivativeTs = ctrl_params[2];
+        elif pid_form == "parallel":
+            if ctrl_params[1] == 0:
+                self.integralTs = 0.0;
+            else:
+                self.integralTs = self.ctrl_gain / ctrl_params[1];
+            self.derivativeTs = ctrl_params[2] / self.ctrl_gain;
+        else:
+            raise Exception( "PID form is either standard or parallel." );
+        
+        # in either form:
+        if self.integralTs == 0:
+            self.use_integrator = False;
+        else:
+            self.use_integrator = True;
+        
+        self.integrator_hist = 5;
+        self.integrator_windup_cutoff = 2;
+        # containers for integral & derivative control
+        self.vel_errs = []
+        self.prev_err = 0.0;
+
+        # do other initialisation bits
+        self.initialise();
+    
+    def initialise(self):
+        # initialise containter for integrator only if I-gain is not strictly zero
+        if self.use_integrator:
+            self.vel_errs = np.zeros(self.integrator_hist);
+
+    def reset(self):
+        self.vel_errs = np.zeros(self.integrator_hist);
+    
+    def integralError(self, err, _idx=[0]):
+        if not use_integrator:
+            return 0;
+        # update integrator container as a fast ring buffer
+        self.vel_errs[_idx[0]] = err;
+        _idx[0] = (_idx[0] + 1) % self.integrator_hist;
+        return (1.0/self.integralTs)*np.sum( self.vel_errs ) * self.dt;
+    
+    def rateError(self, err):
+        e = self.derivativeTs * (err - self.prev_err)/self.dt;
+        self.prev_err = err;
+        return e;
+            
 
     def process_force(self):
-        desired_velocity = self.agent.action.u
-        pos = self.agent.state.pos
-        vel = self.agent.state.vel
+        des_vel = self.agent.action.u;
+        cur_vel = self.agent.state.vel;
 
-        # Appending to the ques
-        self.positions.append(pos)
-        self.velocities.append(vel)
-
-        # Controller
-        force = ((desired_velocity - vel) / self.dt) * self.agent.mass
+        # apply control
+        err = des_vel - cur_vel;
+        u = ctrl_params[0] * ( err + self.integralError(err) + self.rateError(err) );
+        u = u * self.agent.mass;
 
         # Clamping force to limits
         if self.agent.max_f is not None:
@@ -31,9 +78,4 @@ class VelocityController:
         if self.agent.f_range is not None:
             force = torch.clamp(force, -self.agent.f_range, self.agent.f_range)
 
-        if len(self.positions) > 5:
-            self.positions.pop(0)
-        if len(self.velocities) > 5:
-            self.velocities.pop(0)
-
-        self.agent.action.u = force
+        self.agent.action.u = u;
