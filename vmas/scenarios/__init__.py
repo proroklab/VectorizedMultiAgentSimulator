b7b6eef684ae7038ba75c2df63aae7d3bad98814
Matteo Bettini <mb2389@cl.cam.ac.uk>
2022-11-19 16:50:50 +0000

    Fix to scenario loading

--- vmas/scenarios/__init__.py
+++ vmas/scenarios/__init__.py
@@ -1,15 +1,24 @@
 #  Copyright (c) 2022.
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
-
 import importlib
+import os
 import os.path as osp
 
 
 def load(name: str):
-    if name.startswith("simple"):
-        name = "mpe/" + name
-    pathname = osp.join(osp.dirname(__file__), name)
+    if "/" in name:
+        pathname = name
+    else:
+        pathname = None
+        for dirpath, dirnames, filenames in os.walk(osp.dirname(__file__)):
+            if pathname is None:
+                for filename in filenames:
+                    if filename.endswith(name):
+                        pathname = dirpath + "/" + filename
+                        break
+        assert pathname is not None, f"{name} scenario not found."
+
     spec = importlib.util.spec_from_file_location("", pathname)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
