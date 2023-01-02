4b9771a4f0531f782db959891f1c1499638dbd74
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-01-02 16:36:40 +0100

    Added scneario names

--- vmas/__init__.py
+++ vmas/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2022.
+#  Copyright (c) 2022-2023.
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
 
@@ -10,4 +10,60 @@ from vmas.simulator.utils import _init_pyglet_device
 
 _init_pyglet_device()
 
-__all__ = ["make_env", "render_interactively", "Wrapper"]
+__all__ = [
+    "make_env",
+    "render_interactively",
+    "Wrapper",
+    "scenarios",
+    "debug_scenarios",
+    "mpe_scenarios",
+]
+
+scenarios = sorted(
+    [
+        "dropout",
+        "dispersion",
+        "transport",
+        "reverse_transport",
+        "give_way",
+        "wheel",
+        "balance",
+        "football",
+        "discovery",
+        "flocking",
+        "passage",
+        "joint_passage_size",
+        "joint_passage",
+        "ball_passage",
+        "ball_trajectory",
+        "buzz_wire",
+        "multi_give_way",
+        "navigation",
+    ]
+)
+
+debug_scenarios = sorted(
+    [
+        "asym_joint",
+        "circle_trajectory",
+        "goal",
+        "het_mass",
+        "line_trajectory",
+        "vel_control",
+        "waterfall",
+    ]
+)
+
+mpe_scenarios = sorted(
+    [
+        "simple",
+        "simple_adversary",
+        "simple_crypto",
+        "simple_push",
+        "simple_reference",
+        "simple_speaker_listener",
+        "simple_spread",
+        "simple_tag",
+        "simple_world_comm",
+    ]
+)
