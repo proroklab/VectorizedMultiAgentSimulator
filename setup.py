13c614f869d623647f8e3611d25c864eac708ec6
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-04-06 10:30:57 +0200

    VMAS-1.2.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vmas",
-    version="1.2.8",
+    version="1.2.9",
     description="Vectorized Multi-Agent Simulator",
     url="https://github.com/proroklab/VectorizedMultiAgentSimulator",
     license="GPLv3",
