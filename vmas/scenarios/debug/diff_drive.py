6237fa92e07e9a282d0d41c83c499983022c5c8c
Matteo Bettini <mb2389@cl.cam.ac.uk>
2023-05-18 12:26:35 +0100

    amend

--- vmas/scenarios/debug/diff_drive.py
+++ vmas/scenarios/debug/diff_drive.py
@@ -1,16 +1,19 @@
 #  Copyright (c) 2022-2023.
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
+import typing
 from typing import List
 
 import torch
 
 from vmas import render_interactively
 from vmas.simulator.core import Agent, World
-from vmas.simulator.diff_drive import DiffDriveDynamics
-from vmas.simulator.rendering import Geom
+from vmas.simulator.dynamics.diff_drive import DiffDriveDynamics
 from vmas.simulator.scenario import BaseScenario
 from vmas.simulator.utils import Color
+
+if typing.TYPE_CHECKING:
+    from vmas.simulator.rendering import Geom
 
 
 class Scenario(BaseScenario):
@@ -29,9 +32,9 @@ class Scenario(BaseScenario):
             u_rot_range=1,
             u_rot_multiplier=0.001,
         )
+        agent.dynamics = DiffDriveDynamics(agent, world, integration="rk4")
 
         world.add_agent(agent)
-        self.dynamics = DiffDriveDynamics(agent, world)
 
         return world
 
@@ -39,7 +42,8 @@ class Scenario(BaseScenario):
         pass
 
     def process_action(self, agent: Agent):
-        self.dynamics.process_force()
+        for agent in self.world.agents:
+            agent.dynamics.process_force()
 
     def reward(self, agent: Agent):
         return torch.zeros(self.world.batch_dim)
@@ -59,14 +63,13 @@ class Scenario(BaseScenario):
 
         geoms: List[Geom] = []
 
-        # Communication lines
+        # Agent rotation
         color = Color.BLACK.value
         line = rendering.Line(
             (0, 0),
             (0.1, 0),
             width=1,
         )
-
         xform = rendering.Transform()
         xform.set_rotation(self.world.agents[0].state.rot[env_index])
         xform.set_translation(*self.world.agents[0].state.pos[env_index])
@@ -78,4 +81,4 @@ class Scenario(BaseScenario):
 
 
 if __name__ == "__main__":
-    render_interactively(__file__, control_two_agents=False)
+    render_interactively(__file__)
---
+++ vmas/simulator/dynamics/__init__.py
@@ -0,0 +1,3 @@
+#  Copyright (c) 2023.
+#  ProrokLab (https://www.proroklab.org/)
+#  All rights reserved.
