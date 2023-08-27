8862d67c422d4b80378c4b65b5007b4e4146afea
Matteo Bettini <matbet@meta.com>
2023-08-27 10:58:10 +0100

    improve scenario loading to avoid conflicts

--- vmas/scenarios/__init__.py
+++ vmas/scenarios/__init__.py
@@ -7,17 +7,14 @@ import os.path as osp
 
 
 def load(name: str):
-    if os.path.isfile(name):
-        pathname = name
-    else:
-        pathname = None
-        for dirpath, dirnames, filenames in os.walk(osp.dirname(__file__)):
-            if pathname is None:
-                for filename in filenames:
-                    if filename == name:
-                        pathname = os.path.join(dirpath, filename)
-                        break
-        assert pathname is not None, f"{name} scenario not found."
+    pathname = None
+    for dirpath, dirnames, filenames in os.walk(osp.dirname(__file__)):
+        if pathname is None:
+            for filename in filenames:
+                if filename == name:
+                    pathname = os.path.join(dirpath, filename)
+                    break
+    assert pathname is not None, f"{name} scenario not found."
 
     spec = importlib.util.spec_from_file_location("", pathname)
     module = importlib.util.module_from_spec(spec)
