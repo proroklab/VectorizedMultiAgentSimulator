999119d12f850f6b3a19d1023a564b2a51c03854
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-01-17 09:24:24 +0000

    Amend

--- vmas/scenarios/transport.py
+++ vmas/scenarios/transport.py
@@ -164,7 +164,7 @@ class Scenario(BaseScenario):
 
         return torch.cat(
             [
-                # agent.state.pos,
+                agent.state.pos,
                 agent.state.vel,
                 *package_obs,
             ],
