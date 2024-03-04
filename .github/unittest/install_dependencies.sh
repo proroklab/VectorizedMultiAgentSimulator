802fd77e5b8af716f0dd54c49fbbd45e19733fb6
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-03-04 15:47:43 +0000

    amend

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -6,6 +6,6 @@
 
 
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest pytest-cov tqdm
+python -m pip install flake8 pytest pytest-cov tqdm opencv-python
 
 pip install -e .
--- tests/test_use_vmas_env.py
+++ tests/test_use_vmas_env.py
@@ -1,15 +1,37 @@
 #  Copyright (c) 2022-2024.
 #  ProrokLab (https://www.proroklab.org/)
 #  All rights reserved.
+import os
 
+from pathlib import Path
 
 import pytest
-from vmas import debug_scenarios, mpe_scenarios, scenarios
 
 from vmas.examples.use_vmas_env import use_vmas_env
 
 
-@pytest.mark.parametrize("scenario", scenarios + mpe_scenarios + debug_scenarios)
+def scenario_names():
+    scenarios = []
+    scenarios_folder = Path(__file__).parent.parent / "vmas" / "scenarios"
+    for _, _, filenames in os.walk(scenarios_folder):
+        scenarios += filenames
+    scenarios = [
+        scenario.split(".")[0]
+        for scenario in scenarios
+        if scenario.endswith(".py") and not scenario.startswith("__")
+    ]
+    return scenarios
+
+
+def test_all_scenarios_included():
+    from vmas import debug_scenarios, mpe_scenarios, scenarios
+
+    assert sorted(scenario_names()) == sorted(
+        scenarios + mpe_scenarios + debug_scenarios
+    )
+
+
+@pytest.mark.parametrize("scenario", scenario_names())
 @pytest.mark.parametrize("continuous_actions", [True, False])
 def test_use_vmas_env(scenario, continuous_actions, num_envs=10, n_steps=10):
     use_vmas_env(
