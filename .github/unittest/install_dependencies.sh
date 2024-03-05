a10d6edf8032cbf4abb4b166f2a667c51fbdb506
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-03-05 09:35:51 +0000

    amend

--- .github/unittest/install_dependencies.sh
+++ .github/unittest/install_dependencies.sh
@@ -6,6 +6,7 @@
 
 
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest pytest-cov tqdm opencv-python
+python -m pip install flake8 pytest pytest-cov tqdm
 
+cd ../VectorizedMultiAgentSimulator
 pip install -e .
