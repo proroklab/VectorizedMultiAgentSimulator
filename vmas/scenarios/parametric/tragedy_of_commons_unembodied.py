ee85f62b78956d214ac5a72d3b6b8c267fee2a1d
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-03-20 14:47:06 +0000

    amend

--- vmas/scenarios/parametric/tragedy_of_commons_unembodied.py
+++ vmas/scenarios/parametric/tragedy_of_commons_unembodied.py
@@ -39,7 +39,7 @@ class Scenario(BaseScenario):
                 dtype=torch.float,
             )
         )
-        self.gen_agg_type = kwargs.get("gen_agg_type", "mean")
+        self.gen_agg_type = kwargs.get("gen_agg_type", None)
         if self.gen_agg_type is None:
             self.gen_agg = GenAgg().to(device)
         elif self.gen_agg_type == "max":
