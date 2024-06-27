482a5ec76ca22dcca00d366022037d2398c5d7ca
Matteo Bettini <55539777+matteobettini@users.noreply.github.com>
2024-06-27 15:06:12 +0100

    Update vmas/simulator/sensors.py

--- vmas/simulator/sensors.py
+++ vmas/simulator/sensors.py
@@ -135,7 +135,7 @@ class Lidar(Sensor):
                 xform.set_translation(*self.agent.state.pos[env_index])
                 xform.set_rotation(angle)
                 ray.add_attr(xform)
-                ray.set_color(*Color.BLACK.value, alpha=self.alpha)
+                ray.set_color(r=0, g=0, b=0, alpha=self.alpha)
 
                 ray_circ = rendering.make_circle(0.01)
                 ray_circ.set_color(*self.render_color, alpha=self.alpha)
