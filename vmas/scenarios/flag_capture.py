3894d630f948dce42fd1bd08aa0a8ca2fbe58366
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-12-22 11:24:10 +0100

    flag cap

--- vmas/scenarios/flag_capture.py
+++ vmas/scenarios/flag_capture.py
@@ -5,6 +5,7 @@
 import typing
 
 import torch
+from torch_geometric.nn import SoftmaxAggregation
 
 from vmas import render_interactively
 from vmas.simulator.core import Agent, Box, Landmark, Sphere, World
@@ -15,11 +16,51 @@ if typing.TYPE_CHECKING:
     pass
 
 
+def agg_max(x, dim):
+    return x.max(dim=dim, keepdim=True)[0]
+
+
+def agg_min(x, dim):
+    return x.min(dim=dim, keepdim=True)[0]
+
+
+def agg_mean(x, dim):
+    return x.mean(dim=dim, keepdim=True)
+
+
+def agg_sum(x, dim):
+    return x.sum(dim=dim, keepdim=True)
+
+
+class Square:
+    def forward(self, x):
+        return (x + 1e-7) ** 2
+
+    def inverse(self, x):
+        return (x.abs() + 1e-7).sqrt()
+
+
+def get_aggregation_function(name, device):
+    if name == "softmax":
+        return SoftmaxAggregation(t=1, learn=True).to(device)
+    elif name == "max":
+        return agg_max
+    elif name == "mean":
+        return agg_mean
+    elif name == "min":
+        return agg_min
+    elif name == "sum":
+        return agg_sum
+    else:
+        raise AssertionError
+
+
 class Scenario(BaseScenario):
     def make_world(self, batch_dim: int, device: torch.device, **kwargs):
         self.plot_grid = False
         self.n_agents = kwargs.pop("n_agents", 2)
         self.n_adversaries = kwargs.pop("n_adversaries", 0)
+        self.spawn_agents_in_same_pos = kwargs.pop("spawn_agents_in_same_pos", True)
 
         self.n_flags = kwargs.pop("n_flags", 2)
         self.n_adversary_flags = kwargs.pop("n_adversary_flags", 0)
@@ -31,9 +72,15 @@ class Scenario(BaseScenario):
         self.agent_radius = kwargs.pop("agent_radius", 0.05)
 
         self.pos_shaping_factor = kwargs.pop("pos_shaping_factor", 1)
-        self.flag_capture_reward = kwargs.pop("flag_capture_reward", 1)
-        self.flag_drop_reward = kwargs.pop("flag_drop_reward", 1)
-        self.flag_return_reward = kwargs.pop("flag_return_reward", 1)
+        self.flag_capture_reward = kwargs.pop("flag_capture_reward", 0)
+        self.flag_drop_reward = kwargs.pop("flag_drop_reward", 0)
+        self.flag_return_reward = kwargs.pop("flag_return_reward", 0)
+
+        self.gen_agg_type_task = kwargs.pop("gen_agg_type_task", "max")
+        self.gen_agg_type_agent = kwargs.pop("gen_agg_type_agent", "max")
+
+        self.task_agg = get_aggregation_function(self.gen_agg_type_task, device)
+        self.agent_agg = get_aggregation_function(self.gen_agg_type_agent, device)
 
         ScenarioUtils.check_kwargs_consumed(kwargs)
 
@@ -76,15 +123,48 @@ class Scenario(BaseScenario):
 
         return world
 
+    def parameters(self) -> typing.List:
+        params = []
+        if hasattr(self.task_agg, "parameters"):
+            params += self.task_agg.parameters()
+        if hasattr(self.agent_agg, "parameters"):
+            params += self.agent_agg.parameters()
+        return params
+
+    def to_log(self) -> typing.Dict:
+        result = {}
+        if hasattr(self.task_agg, "t"):
+            result["task_agg_t"] = self.task_agg.t.item()
+        if hasattr(self.agent_agg, "t"):
+            result["agent_agg_t"] = self.agent_agg.t.item()
+
+        return result
+
     def reset_world_at(self, env_index: int = None):
-        ScenarioUtils.spawn_entities_randomly(
-            self.blue_agents,
-            self.world,
-            env_index,
-            self.min_distance_between_entities,
-            (-self.world_spawning_x - self.base_width, -self.world_spawning_x),
-            (-self.world_spawning_y, self.world_spawning_y),
-        )
+        if self.spawn_agents_in_same_pos:
+            x = torch.zeros(
+                (1,) if env_index is not None else (self.world.batch_dim, 1),
+                device=self.world.device,
+                dtype=torch.float32,
+            ).uniform_(-self.world_spawning_x - self.base_width, -self.world_spawning_x)
+            y = torch.zeros(
+                (1,) if env_index is not None else (self.world.batch_dim, 1),
+                device=self.world.device,
+                dtype=torch.float32,
+            ).uniform_(-self.world_spawning_y, self.world_spawning_y)
+            pos = torch.cat([x, y], dim=-1)
+            for agent in self.blue_agents:
+                agent.set_pos(pos, batch_index=env_index)
+        else:
+            ScenarioUtils.spawn_entities_randomly(
+                self.blue_agents,
+                self.world,
+                env_index,
+                self.min_distance_between_entities,
+                (-self.world_spawning_x - self.base_width, -self.world_spawning_x),
+                (-self.world_spawning_y, self.world_spawning_y),
+            )
+
         ScenarioUtils.spawn_entities_randomly(
             self.blue_flags,
             self.world,
@@ -93,8 +173,10 @@ class Scenario(BaseScenario):
             (self.world_spawning_x, self.world_spawning_x + self.base_width),
             (-self.world_spawning_y, self.world_spawning_y),
         )
-
-        self.flag_distances = self._get_distance_to_flags(env_index)
+        if env_index is None:
+            self.flag_distances = self._get_distance_to_flags(env_index)
+        else:
+            self.flag_distances[env_index] = self._get_distance_to_flags(env_index)
 
     def reward(self, agent: Agent):
         is_first = agent == self.world.agents[0]
@@ -106,8 +188,9 @@ class Scenario(BaseScenario):
             ) * self.pos_shaping_factor
             self.flag_distances = flag_distances
 
-        rew = self.flag_rews.mean(-1).max(-1)[0]
-        return rew
+        task_matrix = self.agent_agg(-self.flag_distances, dim=-2).squeeze(-2)
+        self.rew = self.task_agg(task_matrix, dim=-1).squeeze(-1)
+        return self.rew
 
     def _get_distance_to_flags(self, env_index: typing.Optional[int] = None):
         if env_index is None:
---
+++ vmas/scenarios/parametric/flag_capture_unembodied.py
@@ -0,0 +1,151 @@
+#  Copyright (c) 2024.
+#  ProrokLab (https://www.proroklab.org/)
+#  All rights reserved.
+from typing import Dict, List
+
+import torch
+
+from torch_geometric.nn import SoftmaxAggregation
+
+from vmas import render_interactively
+from vmas.simulator.core import Agent, World
+from vmas.simulator.dynamics.static import Static
+from vmas.simulator.scenario import BaseScenario
+from vmas.simulator.utils import ScenarioUtils
+
+
+def agg_max(x, dim):
+    return x.max(dim=dim, keepdim=True)[0]
+
+
+def agg_min(x, dim):
+    return x.min(dim=dim, keepdim=True)[0]
+
+
+def agg_mean(x, dim):
+    return x.mean(dim=dim, keepdim=True)
+
+
+def agg_sum(x, dim):
+    return x.sum(dim=dim, keepdim=True)
+
+
+class Square:
+    def forward(self, x):
+        return (x + 1e-7) ** 2
+
+    def inverse(self, x):
+        return (x.abs() + 1e-7).sqrt()
+
+
+def get_aggregation_function(name, device):
+    if name == "softmax":
+        return SoftmaxAggregation(t=1, learn=True).to(device)
+    elif name == "max":
+        return agg_max
+    elif name == "mean":
+        return agg_mean
+    elif name == "min":
+        return agg_min
+    elif name == "sum":
+        return agg_sum
+    else:
+        raise AssertionError
+
+
+class Scenario(BaseScenario):
+    def make_world(self, batch_dim: int, device: torch.device, **kwargs):
+        self.n_agents = kwargs.pop("n_agents", 2)
+        self.task_rewards = kwargs.pop("task_rewards", [1.0, 1.0])
+        self.n_tasks = len(self.task_rewards)
+
+        self.gen_agg_type_task = kwargs.pop("gen_agg_type_task", "softmax")
+        self.gen_agg_type_agent = kwargs.pop("gen_agg_type_agent", "softmax")
+
+        self.task_agg = get_aggregation_function(self.gen_agg_type_task, device)
+        self.agent_agg = get_aggregation_function(self.gen_agg_type_agent, device)
+
+        ScenarioUtils.check_kwargs_consumed(kwargs)
+
+        # Make world
+        world = World(
+            batch_dim,
+            device,
+        )
+
+        # Add agents
+        for i in range(self.n_agents):
+            # Constraint: all agents have same action range and multiplier
+            agent = Agent(
+                name=f"agent_{i}",
+                collide=False,
+                action_size=1,
+                discrete_action_nvec=[self.n_tasks + 1],
+                u_range=self.n_tasks / 2,
+                dynamics=Static(),
+            )
+            world.add_agent(agent)
+
+        return world
+
+    def parameters(self) -> List:
+        params = []
+        if hasattr(self.task_agg, "parameters"):
+            params += self.task_agg.parameters()
+        if hasattr(self.agent_agg, "parameters"):
+            params += self.agent_agg.parameters()
+        return params
+
+    def to_log(self) -> Dict:
+
+        result = {}
+
+        if hasattr(self.task_agg, "t"):
+            result["task_agg_t"] = self.task_agg.t.item()
+        if hasattr(self.agent_agg, "t"):
+            result["agent_agg_t"] = self.agent_agg.t.item()
+
+        return result
+
+    def reset_world_at(self, env_index: int = None):
+        pass
+
+    def process_action(self, agent: Agent):
+        # Actions are integers
+        agent.discrete_action = (
+            (agent.action.u + agent.action.u_range).squeeze(-1).to(torch.int)
+        )
+        agent.action.u = torch.zeros(
+            (self.world.batch_dim, agent.dynamics.needed_action_size),
+            device=self.world.device,
+            dtype=torch.float,
+        )
+
+    def reward(self, agent: Agent):
+        is_first = agent == self.world.agents[0]
+
+        if is_first:
+            reward_matrix = torch.zeros(
+                self.world.batch_dim,
+                self.n_agents,
+                self.n_tasks,
+                device=self.world.device,
+            )
+            for i, a in enumerate(self.world.agents):
+                for j in range(self.n_tasks):
+                    reward_matrix[:, i, j] = self.task_rewards[j] * (
+                        a.discrete_action == j
+                    )
+            task_matrix = self.agent_agg(reward_matrix, dim=-2).squeeze(-2)
+            self.rew = self.task_agg(task_matrix, dim=-1).squeeze(-1)
+
+        return self.rew
+
+    def observation(self, agent: Agent):
+        return torch.zeros(
+            self.world.batch_dim, 1, dtype=torch.float, device=self.world.device
+        )
+
+
+if __name__ == "__main__":
+    render_interactively(__file__, control_two_agents=True)
