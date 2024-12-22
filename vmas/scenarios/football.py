ebb451ca510f0e4b61ce62ac5aa4b09974339e33
Matteo Bettini <mb2389@cl.cam.ac.uk>
2024-12-22 11:47:43 +0100

    param docs

--- vmas/scenarios/football.py
+++ vmas/scenarios/football.py
@@ -28,6 +28,103 @@ if typing.TYPE_CHECKING:
 
 
 class Scenario(BaseScenario):
+    def init_params(self, **kwargs):
+        # Scenario config
+        self.viewer_size = kwargs.pop("viewer_size", (1200, 800))
+
+        # Agents config
+        self.n_blue_agents = kwargs.pop("n_blue_agents", 3)
+        self.n_red_agents = kwargs.pop("n_red_agents", 3)
+        # What agents should be learning and what controlled by the heuristic (ai)
+        self.ai_red_agents = kwargs.pop("ai_red_agents", True)
+        self.ai_blue_agents = kwargs.pop("ai_blue_agents", False)
+
+        # When you have 5 blue agents there is the options of introducing physical differences with the following roles:
+        # 1 goalkeeper -> slow and big
+        # 2 defenders -> normal size and speed (agent_size, u_multiplier, max_speed)
+        # 2 attackers -> small and fast
+        self.physically_different = kwargs.pop("physically_different", False)
+
+        # Agent spawning
+        self.spawn_in_formation = kwargs.pop("spawn_in_formation", False)
+        self.only_blue_formation = kwargs.pop(
+            "only_blue_formation", True
+        )  # Only spawn blue agents in formation
+        self.formation_agents_per_column = kwargs.pop("formation_agents_per_column", 2)
+        self.randomise_formation_indices = kwargs.pop(
+            "randomise_formation_indices", False
+        )  # If False, each agent will always be in the same formation spot
+        self.formation_noise = kwargs.pop(
+            "formation_noise", 0.2
+        )  # Noise on formation positions
+
+        # Ai config
+        self.n_traj_points = kwargs.pop(
+            "n_traj_points", 0
+        )  # Number of spline trajectory points to plot for heuristic (ai) agents
+        self.ai_speed_strength = kwargs.pop(
+            "ai_strength", 1.0
+        )  # The speed of the ai 0<=x<=1
+        self.ai_decision_strength = kwargs.pop(
+            "ai_decision_strength", 1.0
+        )  # The decision strength of the ai 0<=x<=1
+        self.ai_precision_strength = kwargs.pop(
+            "ai_precision_strength", 1.0
+        )  # The precision strength of the ai 0<=x<=1
+        self.disable_ai_red = kwargs.pop("disable_ai_red", False)
+
+        # Task sizes
+        self.agent_size = kwargs.pop("agent_size", 0.025)
+        self.goal_size = kwargs.pop("goal_size", 0.35)
+        self.goal_depth = kwargs.pop("goal_depth", 0.1)
+        self.pitch_length = kwargs.pop("pitch_length", 3.0)
+        self.pitch_width = kwargs.pop("pitch_width", 1.5)
+        self.ball_mass = kwargs.pop("ball_mass", 0.25)
+        self.ball_size = kwargs.pop("ball_size", 0.02)
+
+        # Actions
+        self.u_multiplier = kwargs.pop("u_multiplier", 0.1)
+
+        # Actions shooting
+        self.enable_shooting = kwargs.pop(
+            "enable_shooting", False
+        )  # Whether to enable an extra 2 actions (for rotation and shooting). Only avaioable for non-ai agents
+        self.u_rot_multiplier = kwargs.pop("u_rot_multiplier", 0.0003)
+        self.u_shoot_multiplier = kwargs.pop("u_shoot_multiplier", 0.6)
+        self.shooting_radius = kwargs.pop("shooting_radius", 0.08)
+        self.shooting_angle = kwargs.pop("shooting_angle", torch.pi / 2)
+
+        # Speeds
+        self.max_speed = kwargs.pop("max_speed", 0.15)
+        self.ball_max_speed = kwargs.pop("ball_max_speed", 0.3)
+
+        # Rewards
+        self.dense_reward = kwargs.pop("dense_reward", True)
+        self.pos_shaping_factor_ball_goal = kwargs.pop(
+            "pos_shaping_factor_ball_goal", 10.0
+        )  # Reward for moving the ball towards the opponents' goal. This can be annealed in a curriculum.
+        self.pos_shaping_factor_agent_ball = kwargs.pop(
+            "pos_shaping_factor_agent_ball", 0.1
+        )  # Reward for moving the closest agent to the ball in a team closer to it.
+        # This is useful for exploration and can be annealed in a curriculum.
+        # This reward does not trigger if the agent is less than distance_to_ball_trigger from the ball or the ball is moving
+        self.distance_to_ball_trigger = kwargs.pop("distance_to_ball_trigger", 0.4)
+        self.scoring_reward = kwargs.pop(
+            "scoring_reward", 100.0
+        )  # Discrete reward for scoring
+
+        # Observations
+        self.observe_teammates = kwargs.pop("observe_teammates", True)
+        self.observe_adversaries = kwargs.pop("observe_adversaries", True)
+        self.dict_obs = kwargs.pop("dict_obs", False)
+
+        if kwargs.pop("dense_reward_ratio", None) is not None:
+            raise ValueError(
+                "dense_reward_ratio in football is deprecated, please use `dense_reward` "
+                "which is a bool that turns on/off the dense reward"
+            )
+        ScenarioUtils.check_kwargs_consumed(kwargs)
+
     def make_world(self, batch_dim: int, device: torch.device, **kwargs):
         self.init_params(**kwargs)
         world = self.init_world(batch_dim, device)
@@ -37,6 +134,8 @@ class Scenario(BaseScenario):
         self.init_walls(world)
         self.init_goals(world)
         self.init_traj_pts(world)
+
+        # Cached values
         self.left_goal_pos = torch.tensor(
             [-self.pitch_length / 2 - self.ball_size / 2, 0],
             device=device,
@@ -76,79 +175,6 @@ class Scenario(BaseScenario):
             self._done[:] = False
         else:
             self._done[env_index] = False
-
-    def init_params(self, **kwargs):
-        # Scenario config
-        self.viewer_size = kwargs.pop("viewer_size", (1200, 800))
-
-        # Agents config
-        self.n_blue_agents = kwargs.pop("n_blue_agents", 3)
-        self.n_red_agents = kwargs.pop("n_red_agents", 3)
-        self.ai_red_agents = kwargs.pop("ai_red_agents", True)
-        self.ai_blue_agents = kwargs.pop("ai_blue_agents", False)
-        self.physically_different = kwargs.pop("physically_different", False)
-
-        # Agent spawning
-        self.spawn_in_formation = kwargs.pop("spawn_in_formation", False)
-        self.only_blue_formation = kwargs.pop("only_blue_formation", True)
-        self.formation_agents_per_column = kwargs.pop("formation_agents_per_column", 2)
-        self.randomise_formation_indices = kwargs.pop(
-            "randomise_formation_indices", False
-        )
-        self.formation_noise = kwargs.pop("formation_noise", 0.2)
-
-        # Ai config
-        self.n_traj_points = kwargs.pop("n_traj_points", 0)
-        self.ai_speed_strength = kwargs.pop("ai_strength", 1.0)
-        self.ai_decision_strength = kwargs.pop("ai_decision_strength", 1.0)
-        self.ai_precision_strength = kwargs.pop("ai_precision_strength", 1.0)
-        self.disable_ai_red = kwargs.pop("disable_ai_red", False)
-
-        # Task sizes
-        self.agent_size = kwargs.pop("agent_size", 0.025)
-        self.goal_size = kwargs.pop("goal_size", 0.35)
-        self.goal_depth = kwargs.pop("goal_depth", 0.1)
-        self.pitch_length = kwargs.pop("pitch_length", 3.0)
-        self.pitch_width = kwargs.pop("pitch_width", 1.5)
-        self.ball_mass = kwargs.pop("ball_mass", 0.25)
-        self.ball_size = kwargs.pop("ball_size", 0.02)
-
-        # Actions
-        self.u_multiplier = kwargs.pop("u_multiplier", 0.1)
-
-        # Actions shooting
-        self.enable_shooting = kwargs.pop("enable_shooting", False)
-        self.u_rot_multiplier = kwargs.pop("u_rot_multiplier", 0.0003)
-        self.u_shoot_multiplier = kwargs.pop("u_shoot_multiplier", 0.6)
-        self.shooting_radius = kwargs.pop("shooting_radius", 0.08)
-        self.shooting_angle = kwargs.pop("shooting_angle", torch.pi / 2)
-
-        # Speeds
-        self.max_speed = kwargs.pop("max_speed", 0.15)
-        self.ball_max_speed = kwargs.pop("ball_max_speed", 0.3)
-
-        # Rewards
-        self.dense_reward = kwargs.pop("dense_reward", True)
-        self.pos_shaping_factor_ball_goal = kwargs.pop(
-            "pos_shaping_factor_ball_goal", 10.0
-        )
-        self.pos_shaping_factor_agent_ball = kwargs.pop(
-            "pos_shaping_factor_agent_ball", 0.1
-        )
-        self.distance_to_ball_trigger = kwargs.pop("distance_to_ball_trigger", 0.4)
-        self.scoring_reward = kwargs.pop("scoring_reward", 100.0)
-
-        # Observations
-        self.observe_teammates = kwargs.pop("observe_teammates", True)
-        self.observe_adversaries = kwargs.pop("observe_adversaries", True)
-        self.dict_obs = kwargs.pop("dict_obs", False)
-
-        if kwargs.pop("dense_reward_ratio", None) is not None:
-            raise ValueError(
-                "dense_reward_ratio in football is deprecated, please use `dense_reward` "
-                "which is a bool that turns on/off the dense reward"
-            )
-        ScenarioUtils.check_kwargs_consumed(kwargs)
 
     def init_world(self, batch_dim: int, device: torch.device):
         # Make world
